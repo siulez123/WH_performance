5a59aec0e843c7917e4633fe47d657f74ed2e22e
Jose Luis Pires Ferreira <joseluispires.ferreira@williamhill.com>
2018-11-29 17:23:37 +0100

    asdasdasd

---
+++ cypress/integration/WHWebsite.js
@@ -0,0 +1,15 @@
+context('WilliamHill website scenarios', () => {
+    beforeEach(() => {
+      cy.visit('http://sports.williamhill.com/betting/en-gb')
+    })
+      describe('Login', () {
+        // get loginTabButton()  { return browser.element('#accountTabButton') }
+        // get depositButton() { return browser.element('#depositHeaderButtonLink') }
+
+        it('', () {
+            // browser. .element('#loginButton') }
+            
+         
+          })
+      })
+  })
