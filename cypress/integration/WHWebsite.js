4e9323c83c9d7c2c62a8169930343042760210e1
Jose Luis Pires Ferreira <joseluispires.ferreira@williamhill.com>
2018-11-30 10:56:16 +0100

    first commit

--- cypress/integration/WHWebsite.js
+++ cypress/integration/WHWebsite.js
@@ -1,15 +1,28 @@
-context('WilliamHill website scenarios', () => {
-    beforeEach(() => {
-      cy.visit('http://sports.williamhill.com/betting/en-gb')
+context('WilliamHill - Performance tests', () => {
+  beforeEach(() => {  
+    cy.visit('http://sports.williamhill-pp2.com/betting/en-gb')
+    
+    
     })
-      describe('Login', () {
-        // get loginTabButton()  { return browser.element('#accountTabButton') }
+    // get loginTabButton()  { return browser.element('#accountTabButton') }
         // get depositButton() { return browser.element('#depositHeaderButtonLink') }
-
-        it('', () {
             // browser. .element('#loginButton') }
-            
-         
-          })
+
+      describe('Scenario 1 - Login', function() {
+        it('Login ', function() {
+          cy.get('#accountTabButton').click()
+          cy.get('#loginUsernameInput').type('betslip01')
+          cy.get('#loginPasswordInput').type('Password123')
+          cy.get('#loginButton').click()
+          cy.wait(5000)
+          cy.get('.betbutton__odds:first').click()
+
+        })
       })
   })
+
+
+  // #stake-input__29167177L
+  // #bets-container-singles > div > div
+
+  // #betslip-container .betslip-selection
