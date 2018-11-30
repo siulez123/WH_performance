6badd781ae412b747bcf0dd115bf70bbcd603798
Jose Luis Pires Ferreira <joseluispires.ferreira@williamhill.com>
2018-11-30 15:03:13 +0100

    2 scenarios

--- cypress/integration/WHWebsite.js
+++ cypress/integration/WHWebsite.js
@@ -1,28 +1,33 @@
+
+function login(username, password) {
+  cy.get('#accountTabButton').click()
+  cy.get('#loginUsernameInput').type(username)
+  cy.get('#loginPasswordInput').type(password)
+  cy.get('#loginButton').click()
+  cy.wait(5000)
+}
+
 context('WilliamHill - Performance tests', () => {
   beforeEach(() => {  
     cy.visit('http://sports.williamhill-pp2.com/betting/en-gb')
+    login('betslip01','Password123')
     
     
     })
-    // get loginTabButton()  { return browser.element('#accountTabButton') }
-        // get depositButton() { return browser.element('#depositHeaderButtonLink') }
-            // browser. .element('#loginButton') }
+    describe('Scenario 1 - Track clicking the Open Bets tab', function() {
+      it('Changing between tabs', function() {
+        cy.get('[ng-show="openBetsVersion"]').click()
+        cy.get('#betslip-tab > .betslip-navigation__menu-link > .betslip-navigation__menu-text').click()
+        cy.get('[ng-show="openBetsVersion"]').click()
+      })
+    })
 
-      describe('Scenario 1 - Login', function() {
+      describe('Scenario 2 - Place a bet', function() {
         it('Login ', function() {
-          cy.get('#accountTabButton').click()
-          cy.get('#loginUsernameInput').type('betslip01')
-          cy.get('#loginPasswordInput').type('Password123')
-          cy.get('#loginButton').click()
-          cy.wait(5000)
           cy.get('.betbutton__odds:first').click()
-
+          cy.get('#bets-container-singles > div > div > div:nth-child(3) > span > input').type('1')
+          cy.get('#betslip-footer-actions > ul > li.betslip-bet-actions__list-item.betslip-bet-actions__list-item--full-width > input').click()
+          cy.get('#receipt-notice-box').should('be.visible')
         })
       })
   })
-
-
-  // #stake-input__29167177L
-  // #bets-container-singles > div > div
-
-  // #betslip-container .betslip-selection
